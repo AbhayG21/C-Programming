16388fd406136fc9e10509d8a386afe3b600af6a
AbhayG21 <abhay19021@gmail.com>
2023-01-14 14:19:24 +0530

    Third Commit

---
+++ StoreComplexNo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct complex
+{
+    int real;
+    int imaginary;
+};
+
+int main()
+{
+    int real,img;
+    printf("Enter the Real part: ");
+    scanf("%d", &real);
+    printf("Enter the Imaginary part: ");
+    scanf("%d", &img);
+    struct complex c1 = {real,img};
+    struct complex *ptr = &c1;
+
+    printf("Real part:%d\n", (ptr->real));
+    printf("Imaginary part:%d\n", (ptr->imaginary));
+    printf("Complex number: %d + %di\n",ptr->real,ptr->imaginary);
+
+    return 0;
+}
---
+++ SumOfVectors.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct vector
+{
+    int x;
+    int y;
+};
+
+void calcSum(struct vector v1, struct vector v2, struct vector sum)
+{
+    sum.x = v1.x + v2.x;
+    sum.y = v1.y + v2.y;
+
+    printf("Sum of x is:%d\n",sum.x);
+    printf("Sum of y is:%d\n", sum.y);
+}
+
+int main()
+{
+    int x,y;
+    printf("Enter the coordinates of 1st vector:-\n");
+    printf("x:");
+    scanf("%d",&x);
+    printf("y:");
+    scanf("%d", &y);
+    struct vector v1 = {x, y};
+
+    printf("Enter the coordinates of 2nd vector:-\n");
+    printf("x:");
+    scanf("%d", &x);
+    printf("y:");
+    scanf("%d", &y);
+    struct vector v2 = {x,y};
+
+    struct vector sum = {0};
+
+    calcSum(v1,v2,sum);
+
+    return 0; 
+}
